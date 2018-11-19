a82cae0a3bbbee093deb88aace356ebdd407cce1
Paul Lassen <Paul Lassen>
2018-11-19 22:37:55 +0100

    Added MATLAB startup file

---
+++ startup.m
@@ -0,0 +1,5 @@
+fprintf('Running Startup file...\n')
+set(groot,'defaulttextinterpreter','latex');  
+set(groot, 'defaultAxesTickLabelInterpreter','latex');  
+set(groot, 'defaultLegendInterpreter','latex');
+format shortg
