fa18e2c3a8ffdfa1969c07fcb92bed442a766491
Paul Lassen <paullassen+github@protonmail.com>
2018-11-20 19:06:00 +0100

    Removed startup.m, added git ignore and module 11

--- startup.m
+++
@@ -1,5 +0,0 @@
-fprintf('Running Startup file...\n')
-set(groot,'defaulttextinterpreter','latex');  
-set(groot, 'defaultAxesTickLabelInterpreter','latex');  
-set(groot, 'defaultLegendInterpreter','latex');
-format shortg
